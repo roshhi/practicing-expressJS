a1aabb01c899ae8ff09d71c98d553c3a9dcec9db
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-10-06 05:11:05 +0500

    Created a form to add new members.

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const { engine } = require('express-handlebars');
 const port = process.env.PORT || 1000;
+
+// Handlebars middleware
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:false}))
+
+app.get('/',(req,res)=> res.render('index'));
 
 // Members api routes
 app.use('/api/members',require('./routes/api/members'));
